fd377bf932df219e81ff75743b270717fd839536
Aditya Kumar Choudhary <adityakrchy101@gmail.com>
2023-11-10 13:13:46 +0530

    solved kadane algorithm

--- CodingQuestions/MaximumSubarray.cpp
+++ CodingQuestions/MaximumSubarray.cpp
@@ -1,24 +1,84 @@
 // Given an integer array nums, find the subarray with the largest sum, and return its sum.
 
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    vector<int> arr(n);
-    for(int i=0;i<n;i++) cin>>arr[i];
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     vector<int> arr(n);
+//     for(int i=0;i<n;i++) cin>>arr[i];
 
-    int maxSum = INT_MIN;
+//     int maxSum = INT_MIN;
 
-    for(int i = 0; i<n; i++){
-        for(int j = i; j<n; j++){
-            int sum = 0;
-            for(int k = i; k<=j; k++){
-                sum += arr[k];
-                maxSum = max(maxSum, sum);
-            }
-        }
-    }
+//     for(int i = 0; i<n; i++){
+//         for(int j = i; j<n; j++){
+//             int sum = 0;
+//             for(int k = i; k<=j; k++){
+//                 sum += arr[k];
+//                 maxSum = max(maxSum, sum);
+//             }
+//         }
+//     }
 
-    cout<<maxSum<<endl;
-}
+//     cout<<maxSum<<endl;
+// }
+
+// time complexity: O(n^3)
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     vector<int> arr(n);
+//     for (int i = 0; i < n; i++)
+//         cin >> arr[i];
+
+//     int maxSum = INT_MIN;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         int sum = 0;
+//         for (int j = i; j < n; j++)
+//         {
+//             sum += arr[j];
+//             maxSum = max(maxSum, sum);
+//         }
+//     }
+
+//     cout << maxSum << endl;
+// }
+
+// time complexity: O(n^2)
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     vector<int> arr(n);
+//     for (int i = 0; i < n; i++)
+//         cin >> arr[i];
+
+//     int maxSum = INT_MIN;
+//     int sum = 0;
+//     for (int i = 0; i < n; i++)
+//     {
+//         sum += arr[i];
+//         if (sum > maxSum)
+//         {
+//             maxSum = sum;
+//         }
+
+//         if (sum < 0)
+//         {
+//             sum = 0;
+//         }
+//     }
+//     cout<<maxSum<<endl;
+// }
+
+// time complexity: O(n)
+// kadane's algorithm
